8d55e3323ff08c12c0ed1e4f42480af602214b08
Behlica <niveasouza86@gmail.com>
2025-03-21 12:57:50 -0300

    alteração no script e entendimento no codigo

--- script/main.js
+++ script/main.js
@@ -1,34 +1,38 @@
-const frm = document.querySelector("form");
-const respLista = document.querySelector("pre");
-const respCavalo  = document.querySelector("#outCavalo");
+const frm = document.querySelector("form")
+const outResult = document.querySelector("pre")
+const outHorse = document.querySelector("#outCavalo")
 
-const CAVALOS = ["Bala No Alvo", "Veloz", "Raio", "Twister", "Foguete", "Furia"];
+const horse = ["Shazan", "Bala no Alvo", "Botz", "Foguete", "Brilho do Sol", "Scorpion"]
+const apostas = [];
 
-const apostas = []; 
-
-frm.addEventListener("submit", (e) =>{
-    e.preventDefault();
-
-    const cavalo = Number(frm.inCavalo.value);
-    const valor = Number(frm.inValor.value);
-
-    apostas.push({cavalo, valor});
-
-    let lista = 'Apostas Realizadas\n${"-".repeat(25)\n}';
-
-    for(const aposta of apostas){
-        lista += 'Nº ${aposta.cavalo} ${obterCavalo(aposta.cavalo)}';
-        lista += ' - R$: ${aposta.valor.toFixed(2)}\n';
+const lerListHorse = (horse) =>{
+    let list = "";
+    for (let i = 0; i < horse.length; i++) {
+        list += `${i + 1}- ${horse[i]} || `; 
     }
-    respLista.innerText = lista; 
-
-    frm.reset();
-    frm.inCavalo.focus();
-
-});
-const obterCavalo = (num) =>{
-
-    const posicao = num - 1; 
-    return CAVALOS[posicao];
+    return list; 
 }
+outHorse.innerText = lerListHorse(horse)
 
+frm.addEventListener("submit", (e) => {
+
+    e.preventDefault();
+    const nCavalo = Number(frm.inCavalo.value);
+    const nAposta = Number(frm.inValor.value);
+    
+    apostas.push({nCavalo, nAposta});
+
+    let lista = `Apostas Realizadas\n${"-".repeat(25)}\n`
+    for (const aposta of apostas){
+        lista += `Nº ${aposta.nCavalo} ${obterCavalo(aposta.nCavalo)}`
+        lista +=` - R$: ${aposta.nAposta.toFixed(2)}\n`
+    }
+    outResult.innerText = lista
+    
+    frm.reset()
+    frm.inCavalo.focus()
+})
+const obterCavalo = (num) =>{
+    const posicao = num - 1
+    return horse[posicao] 
+}
